a8339258a3584206408e120add68b405d7d46ff5
danielmarbach <daniel.marbach@openplace.net>
2012-11-06 23:09:50 +0100

    Added some resource metadata

--- Restaurant/GetMenu.cs
+++ Restaurant/GetMenu.cs
@@ -1,15 +1,27 @@
 namespace Restaurant
 {
+    using System;
     using System.Collections.Generic;
 
     using Simple.Web;
     using Simple.Web.Behaviors;
+    using Simple.Web.Http;
 
     [UriTemplate("/menu")]
-    public class GetMenu : IGet, IOutput<List<MenuItem>>
+    public class GetMenu : IGet, IOutput<List<MenuItem>>, IModified, ICacheability
     {
         public GetMenu()
         {
+            var utcNow = DateTime.UtcNow;
+
+            this.LastModified = new DateTime(utcNow.Year, utcNow.Month, utcNow.Day, 0, 0, 0, DateTimeKind.Utc);
+            this.IfModifiedSince = new DateTime(utcNow.Year, utcNow.Month, utcNow.Day, 1, 0, 0, DateTimeKind.Utc);
+
+            this.CacheOptions = new CacheOptions(this.LastModified.Value.Add(TimeSpan.FromDays(1)))
+                {
+                    Level = CacheLevel.Private
+                };
+  
             this.Output = new List<MenuItem>
                 {
                     new MenuItem { Name = "Latte", Price = 5.50m },
@@ -23,6 +35,12 @@ namespace Restaurant
         }
 
         public List<MenuItem> Output { get; private set; }
+
+        public DateTime? IfModifiedSince { set; private get; }
+
+        public DateTime? LastModified { get; private set; }
+
+        public CacheOptions CacheOptions { get; private set; }
     }
 
     public class MenuItem
